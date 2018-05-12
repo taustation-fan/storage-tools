cedba842063889065128f263e797908f66be9d42
Moritz Lenz <moritz.lenz@gmail.com>
2018-05-12 15:02:48 +0200

    stats.pl: mass per station

--- stats.pl
+++ stats.pl
@@ -5,9 +5,10 @@ use warnings;
 use utf8;
 use autodie;
 use JSON qw(decode_json);
+use Encode qw(decode_utf8);
 use lib 'lib';
 
-use Taustation qw(extract_storage_from_html merge_inventory);
+use Taustation qw(extract_storage_from_html merge_inventory extract_station_name) ;
 binmode STDOUT, ':encoding(UTF-8)';
 
 my %all_items;
@@ -18,16 +19,24 @@ unless (@ARGV) {
 
 for my $file (@ARGV) {
     my $inventory = extract_storage_from_html($file)->{carried};
-    merge_inventory(\%all_items, $inventory);
+    my $station = extract_station_name($file);
+    merge_inventory(\%all_items, $inventory, $station);
 }
 
 my $total_mass = 0;
 my $total_value = 0;
+my %mass_by_station;
 
 for my $item (values %all_items) {
     $total_mass += $item->{quantity} * $item->{mass};
     $total_value += $item->{quantity} * $item->{value};
+    while (my ($station, $qty) = each  %{ $item->{by_station} } ) {
+        $mass_by_station{$station} += $item->{mass} * $qty;
+    }
 }
 
-printf "Total mass: %d kg\n", int($total_mass);
 printf "Total value: %d (sells for %d)\n", int($total_value), int($total_value*0.3);
+for my $station (sort keys %mass_by_station) {
+    printf "    %20s: %d kg\n", decode_utf8($station), $mass_by_station{$station};
+}
+printf "              TOTAL MASS: %d kg\n", int($total_mass);
