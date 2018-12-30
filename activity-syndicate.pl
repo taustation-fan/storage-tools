4c56647a2e7de815438ebf39c91708c3dd1af77b
Moritz Lenz <moritz.lenz@gmail.com>
2018-12-30 20:43:44 +0100

    Measure syndicate activity by score

--- activity-syndicate.pl
+++ activity-syndicate.pl
@@ -9,20 +9,46 @@ use JSON::XS qw(encode_json);
 use File::Path qw(make_path);
 use List::Util qw(sum);
 use Data::Dumper;
+use Date::Simple qw(today);
 
 my %all_data;
 my %dates;
 
+my $today = today();
+my $first = $today - 20;
+
+my %weight = (
+    bonds       => 20,
+    level       => 1000,
+    credits     => 0.2,
+    stats       => 100,
+    couse_count => 500,
+);
+
+# whitelist players for which there is a data set today,
+# to avoid grading players that have been removed already
+my %whitelist;
+for my $filename (glob "syndicate/characters/$today/*.html") {
+    if ($filename =~ m{.*/(.+?)\.html}) {
+        $whitelist{$1} = 1;
+    }
+}
+
+
 for my $filename (glob "syndicate/characters/*/*.html") {
     my $character;
-    if ($filename =~ m{.*/(.+?)\.html}) {
-        $character = "$1";
+    my $date;
+    if ($filename =~ m{.*/(\d\d\d\d-\d\d-\d\d)/(.+?)\.html}) {
+        $date = $1;
+        $character = "$2";
     }
     else {
         warn "Don't know what to do with file name '$character'\n";
         break;
     }
-    my ($date, $data) = data_from_file($filename);
+    next unless $whitelist{$character};
+    next if $date lt $first;
+    my (undef, $data) = data_from_file($filename);
     $dates{$date}++;
     my %extracted = %{ $data->{extracted} };
     my $stats = sum values %{ $extracted{stats} };
@@ -35,40 +61,32 @@ for my $filename (glob "syndicate/characters/*/*.html") {
     };
 }
 
-my @keys = qw(stats level bonds credits course_count);
-my @players = sort keys %all_data;
-my @columns = ('Date');
-for my $player (@players) {
-    for my $key (@keys) {
-        push @columns, "$player/$key";
-        push @columns, "$player/$key Δ";
+for my $player (sort keys %all_data) {
+    my @dates = sort keys %{$all_data{$player}};
+    my $score = 0;
+    my $days = 0;
+    for my $idx (1..$#dates) {
+        my ($d1, $d2) = @dates[$idx - 1, $idx];
+        next unless $d1 && $d2;
+        $score += score_diff($all_data{$player}{$d1}, $all_data{$player}{$d2});
+        $days++;
     }
-}
-say join "\t", @columns;
-
-my %previous;
-for my $date (sort keys %dates) {
-    my @row = ($date);
-    for my $player (@players) {
-        for my $key (@keys) {
-            my $value = $all_data{$player}{$date}{$key};
-            if (defined $value) {
-                push @row, $value;
-                my $previous = $previous{$player}{$key};
-                if (defined $previous) {
-                    push @row, sprintf '%.3f', $value - $previous;
-                }
-                else {
-                    push @row, '';
-                }
-                $previous{$player}{$key} = $value;
-            }
-            else {
-                delete $previous{$player}{$key};
-                push @row, '', '';
-            }
-        }
-    }
-    say join "\t", @row;
+    $days ||= 1;
+    $score = int($score / $days);
+    my $inactivity = $score == 0 ? 'INACTIVE' :
+                     $score < 500 ? 'little activity' : '';
+    say $player, "\t", $score, "\t", $inactivity;
 }
 
+sub score_diff {
+    my ($old, $new) = @_;
+    my $score = 0;
+    for my $k (sort keys %weight) {
+        my $weight = $weight{$k};
+        my $old_value = $old->{$k};
+        my $new_value = $new->{$k};
+        next unless defined($new_value) and defined($old);
+        $score += $weight * abs($new_value - $old_value);
+    }
+    return $score;
+}
